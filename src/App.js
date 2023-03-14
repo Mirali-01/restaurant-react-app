6d4b5a6cde969680089c97a152ab529b39a7366a
Mir <mir.m.ali01@gmail.com>
2023-03-14 11:09:22 -0400

    routing restaurant app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Drinks from "./pages/Drinks";
+import Meals from "./pages/Meals";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/drinks" element={<Drinks />} />
+        <Route path="/meals" element={<Meals />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Meals.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Meals = () => {
+  const apiKey = "1";
+
+  return <div>Meals</div>;
+};
+
+export default Meals;
