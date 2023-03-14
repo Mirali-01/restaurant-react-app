fc784550e7c8756f2a8e33b438f7a4db2c10a32e
Mir <mir.m.ali01@gmail.com>
2023-03-14 11:31:42 -0400

    now i fooled myself, from room 1

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const Drinks = () => {
+const Drinks = (props) => {
   const apiKey = "1";
 
-  return <div></div>;
+  const url = `www.thecocktaildb.com/api/json/v1/${apiKey}/search.php?s=${""}`;
+
+  const [drink, setDrink] = useState({
+    searchTerm: "",
+  });
+
+  //handleChange - updates Drink when we type into form
+  const handleChange = (event) => {
+    //use the event object to detect key and value to update
+    setDrink({ ...drink, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    //prevent page from refreshing on form submission
+    event.preventDefault();
+    //pass the search term to moviesearch prop, which is apps getMovie function
+    props.drinkSearch(drink.searchTerm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="search"
+          type="text"
+          name="searchTerm"
+          placeholder="Search drinks here"
+          onChange={handleChange}
+          value={drink.searchTerm}
+        />
+        <input className="submit" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Drinks;
